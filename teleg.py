91d4065b21105101db0dee0ab1d86cc2929fb050
Sabantyi <69809182+Sabantyi@users.noreply.github.com>
2023-07-14 21:03:12 +0500

    Add files via upload

--- teleg.py
+++ teleg.py
@@ -5,7 +5,7 @@ now = datetime.datetime.now()
 def action(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
-    print 'Received: %s' % command
+    print ('Received: %s' % command)
     if command == '/hi':
         telegram_bot.sendMessage (chat_id, str("Hi! CircuitDigest"))
     elif command == '/time':
@@ -19,6 +19,6 @@ def action(msg):
 telegram_bot = telepot.Bot('468382312:AAFhURMxpVlMWEdFzbIQLszBPFEUpAeOLFQ')
 print (telegram_bot.getMe())
 MessageLoop(telegram_bot, action).run_as_thread()
-print 'Up and Running....'
+print ('Up and Running....')
 while 1:
     time.sleep(10)
